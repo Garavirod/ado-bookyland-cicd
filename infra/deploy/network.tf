df358b4e0abcf3a215ba1fafc40989fc4dd43db3
Garavirod <rodrigogarciaavila26@gmail.com>
2024-10-13 16:03:27 -0600

    remove vpc

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -3,11 +3,11 @@
 # #################
 
 // VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.1.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
+# resource "aws_vpc" "main" {
+#   cidr_block           = "10.1.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+# }
 
 # ####################
 # # Internet Gateway #
