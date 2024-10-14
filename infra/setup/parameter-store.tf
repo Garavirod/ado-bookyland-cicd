3449993a7a225f464cc3d31aeb129d5fbabf5a54
Garavirod <rodrigogarciaavila26@gmail.com>
2024-10-13 19:30:40 -0600

    refactor setup and remove ssm

--- infra/setup/parameter-store.tf
+++ infra/setup/parameter-store.tf
@@ -10,12 +10,12 @@ resource "random_password" "database_user_password" {
 }
 
 // Parameter store definition
-resource "aws_ssm_parameter" "database_user_password" {
-  name        = "/${var.application_name}/database/password"
-  description = "Randomly generated database user password"
-  type        = "SecureString"
-  value       = random_password.database_user_password.result
-}
+# resource "aws_ssm_parameter" "database_user_password" {
+#   name        = "/${var.application_name}/database/password"
+#   description = "Randomly generated database user password"
+#   type        = "SecureString"
+#   value       = random_password.database_user_password.result
+# }
 
 resource "aws_ssm_parameter" "dockerhub_token" {
   name        = "/${var.application_name}/dockerhub/token"
